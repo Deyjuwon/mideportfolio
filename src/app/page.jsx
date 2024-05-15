c34bc3316e0439e74507ac399e475005e51d758b
Deyjuwon <temitayoadejuwon@gmail.com>
2024-05-15 11:48:05 +0100

    added animation to the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,12 @@
 import Link from 'next/link';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
+
+
     useEffect(() => {
       AOS.init({duration:2000})
   }, [])
@@ -17,11 +20,13 @@ export default function Home() {
       {/* <Image src={homeBg} alt='home background' className='brightness-50 relative'></Image> */}
       <div className=" text-white flex flex-col justify-center items-center gap-20" >
         
-        <p  data-aos='zoom-in' className='flex flex-col justify-center items-center text-center pt-20 text-white'><span className='text-7xl'>Make A Statement</span> <span className='text-8xl'>WITHOUT SAYING</span><span className='text-9xl'>A WORD</span></p>
+        <p  data-aos='zoom-in' className='flex gap-3 flex-col justify-center items-center text-center pt-40 md:pt-20 text-white'><span className='text-5xl md:text-7xl px-10'>Make A Statement</span> <span className='text-6xl md:text-8xl'>WITHOUT SAYING</span><span className='text-7xl md:text-9xl'>A WORD</span></p>
         <div data-aos='fade-right'>
           <Link href='/collection'><button className='p-4 bg-gradient-to-r from-lightBlack to to-darkBlack border border-gray-400 rounded-3xl flex items-center justify-center w-60 h-16 brightness-150 hover:bg-white hover:text-black hover:scale-110 transition-all duration-500 ease-in'>explore more</button></Link>
 
         </div>
+
+        
         
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
+'use client'
+
 import Link from "next/link"
 import homeBg from '../../public/mide1.jpeg'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 
 const Header = () => {
+
+  const [isOpened, setIsOpened] = useState(false)
+  
   return (
     <div className="flex items-center justify-around py-6">
         <div>
             <p className="text-2xl font-bold logo">AYOMIDE</p>
         </div>
         <nav className="">
-          <ul className="text-sm  font-medium flex gap-8 ">
+          <ul className="text-sm  font-medium hidden md:flex gap-8 ">
             <li className="hover:font-semibold transition hover:scale-110"><Link href="/collection" >Collection</Link></li>
             <li className="hover:font-semibold transition duration-700 ease-in-out hover:scale-110"><Link href="/about" >About Me</Link></li>
             <li className="hover:font-semibold transition duration-700 ease-in-out hover:scale-110"><Link href="/contact" >Contact</Link></li>
@@ -21,9 +29,11 @@ const Header = () => {
             
 
         </nav>
-        <div>
+        <div className="hidden md:block">
             <button className="bg-black text-white py-3 px-4  border rounded-lg text-sm  transition duration-700 ease-in-out hover:scale-110">Book a call</button>
         </div>
+
+        {isOpened ? <div className='bg-white rounded-full flex items-center justify-center h-11 w-11 md:hidden z-50'><IoMdClose className='fixed' size={25} onClick={() => setIsOpened(!isOpened)} /></div> : <div className='bg-white rounded-full flex items-center justify-center h-11 w-11 md:hidden'><RxHamburgerMenu className='' size={25} onClick={() => setIsOpened(!isOpened)} /></div>}
       
     </div>
   )
